257039f96b48d93834ef63f700cf041d6820c2ed
yalpcode <sanyaa1aaa@yandex.ru>
2025-11-16 21:14:26 +0300

    Fix main

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ int main() {
 
     std::vector<int> thresholds = {5, 10, 20, 30, 50};
 
-    // sort_tester.runs(array_generator.getArr1(), "random.csv", thresholds);
+    sort_tester.runs(array_generator.getArr1(), "random.csv", thresholds);
     sort_tester.runs(array_generator.getArr2(), "reverse.csv", thresholds);
-    // sort_tester.runs(array_generator.getArr3(), "almost_sorted.csv",
-    //                  thresholds);
+    sort_tester.runs(array_generator.getArr3(), "almost_sorted.csv",
+                     thresholds);
 
     return 0;
 }
